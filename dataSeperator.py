0e47b165b4436e7d48798fc4759dae0f3ba5b86e
UsmanNiz <82365439+UsmanNiz@users.noreply.github.com>
2022-05-12 21:25:22 +0500

    Committing

---
+++ dataSeperator.py
@@ -0,0 +1,31 @@
+import os
+from random import shuffle
+import shutil
+
+list1 = os.listdir("Data")
+listOfText= []
+
+for value in list1:
+    if "txt" in value and "class" not in value:
+        listOfText.append(value)
+
+shuffle(listOfText)
+
+lengthOfTxt = len(listOfText)
+
+trainListOfTxt = listOfText[:int(lengthOfTxt*0.8)]
+valListOfTxt = listOfText[int(lengthOfTxt*0.8):]
+
+for val in trainListOfTxt:
+    shutil.copy("Data/" + (val),"Litter Detection Dataset/labels/train")
+    shutil.copy(("Data/" + (val.split(".")[0] +".jpg")), "Litter Detection Dataset/images/train" )
+for val in valListOfTxt:
+    shutil.copy("Data/" + (val),"Litter Detection Dataset/labels/val")
+    shutil.copy(("Data/" + (val.split(".")[0] +".jpg")), "Litter Detection Dataset/images/val" )
+
+# os.mkdir("")
+
+# print(len(trainListOfTxt), "  ", len(valListOfTxt))
+# print( len(listOfText),len(list1),listOfText)
+
+# print(list1)
---
+++ frameExtractor.py
@@ -0,0 +1,114 @@
+import cv2
+import os
+
+# cap = cv2.VideoCapture("LITTER/VID20220320142848.mp4")
+pathToListDirectory = ("LITTER/")
+directoryToSaveFrame = ("Data")
+
+
+class Directory:
+    def __init__(self):
+        self.currentDirectory = os.getcwd()
+        # self.directoryName = directoryName
+    # def
+
+    def checkDirectory(self,directoryName):
+        pathToCheckNewDir = os.path.join(self.currentDirectory , directoryName)
+        return os.path.exists(pathToCheckNewDir)
+
+    def makeDirectory(self,directoryName):
+        pathToCreateNewDirectory = os.path.join(self.currentDirectory,directoryName)
+        # if not os.path.exists(pathToCreateNewDirectory):
+        if not self.checkDirectory(directoryName):
+            os.mkdir(pathToCreateNewDirectory)
+            print("Directory Created")
+            # self.currentDirectory = pathToCreateNewDirectory
+
+    def listDirectory(self,path):
+        return os.listdir(path)
+
+
+class boomboom:
+    def __init__(self):
+        self.counter = 0
+
+    def showVideo(self,videoPath):
+        videoPath = "LITTER/" + videoPath
+        cap = cv2.VideoCapture(videoPath)
+        print(videoPath)
+        while True:
+            self.counter = self.counter + 1
+            success, frame = cap.read()
+            if self.counter % 5 == 0 and success:
+                cv2.imshow("Video", frame)
+                cv2.waitKey(1000)
+            if not success:
+                self.counter=0
+                break
+
+    def playingMultipleVideos(self,listOfVideos):
+        for videoPath in listOfVideos:
+            self.showVideo(videoPath)
+
+    def frameWrite(self,videoName, directoryToSaveFrame):
+        videoPath = "LITTER/" + videoName
+        cap = cv2.VideoCapture(videoPath)
+        while True:
+            self.counter = self.counter + 1
+            success, frame = cap.read()
+            if self.counter % 5 == 0 and success:
+                frameName = (videoName.split(".")[0]) + str("#") + str(self.counter / 5).split(".")[0] + str(".jpg")
+                cv2.imwrite(directoryToSaveFrame + "/" + frameName, frame)
+            if not success:
+                self.counter = 0
+                break
+    def frameExtractionFromMultipleVideos(self,listOfVideos):
+        for videoPath in listOfVideos:
+            self.frameWrite(videoPath, directoryToSaveFrame)
+
+
+dirObject = Directory()
+
+listOfVideos = dirObject.listDirectory(pathToListDirectory)
+dirObject.makeDirectory(directoryToSaveFrame)
+
+blow = boomboom()
+blow.frameExtractionFromMultipleVideos(listOfVideos)
+
+
+
+# listOfVideos = os.listdir("LITTER/")
+# # print(videosList)
+# counter = 0
+#
+# dataDirectory = "Data"
+# currentDirectory = os.getcwd()
+#
+# pathToStoreFrame = os.path.join(currentDirectory, dataDirectory)
+#
+# isDir = os.path.isdir(dataDirectory)
+# if not isDir:
+#     os.mkdir(pathToStoreFrame)
+
+# for video in videosList:
+#     print(video)
+# for video in listOfVideos:
+#     # break
+#     path = ("LITTER/" + video)
+#     cap = cv2.VideoCapture(path)
+#     while True:
+#         counter = counter + 1
+#         success, frame = cap.read()
+#         # print(success)
+#         if counter % 10 == 0 and success:
+#             frameName = (video.split(".")[0]) + str("#") + str(counter/10).split(".")[0] +str(".jpg")
+#             print(frameName)
+#             cv2.imshow("video" , frame)
+#             cv2.imwrite(directoryToSaveFrame +"/"+ frameName,frame)
+#             cv2.waitKey(1000)
+#         if not success:
+#             break
+#
+#     counter= 0
+#
+# print("libray imported")
